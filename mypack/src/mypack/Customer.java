116e8368564f9c4bd28504d47b524dddb18c2075
User <User@192.168.0.3>
2020-04-24 11:21:07 +0530

    1st Commit

---
+++ mypack/src/mypack/Customer.java
@@ -0,0 +1,16 @@
+package mypack;
+
+public class Customer {
+
+	public static void main(String[] args) 
+	{
+	  int customerId;
+	  String customerName;
+      double customerMoblieNumber;
+	}
+ 
+	public void addCutomer() 
+	{
+		
+	}
+}
---
+++ mypack/src/mypack/HelloWorld.java
@@ -0,0 +1,8 @@
+package mypack;
+
+public class HelloWorld
+{
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+}
