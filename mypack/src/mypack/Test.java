4f8e8d89327cc5300b7ace1c0574c7e65901cc25
User <User@192.168.0.3>
2020-04-24 11:59:29 +0530

    2 commit

--- mypack/src/mypack/Test.java
+++ mypack/src/mypack/Test.java
@@ -3,7 +3,7 @@ package mypack;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Auto-generated method ");
+		System.out.println("Auto-generated method aa");
 
 	}
 
